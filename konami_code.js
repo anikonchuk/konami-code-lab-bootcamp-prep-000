a5800cee59b5992cf3d1110baf8fe4e7220476aa
Andrew Nikonchuk <anikonch@gmail.com>
2018-04-15 17:14:51 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -6,14 +6,22 @@ let index = 0;
 function init() {
   // Write your JavaScript code inside the init() function
   var body = document.body;
+
+// add event listener
   body.addEventListener('keydown', function(e){
     const key = parseInt(e.detail || e.which);
+
+// check to see if key value is the same as the code at the specified index and increase index if it is;
     if (key === code[index]) {
       index++;
+      
+// if index reaches length of code, complete code achieved and reset index
       if (index === code.length) {
         alert("hurray!");
         index = 0;
       }
+      
+// otherwise reset index
     } else {
       index = 0;
     }
